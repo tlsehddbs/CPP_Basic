72e91c90955c3abc9e4397546618764239323f09
tlsehddbs <sdy4277@naver.com>
2021-01-04 16:55:42 +0900

    Solved

--- 02/PROGRAMMING/05.cpp
+++ 02/PROGRAMMING/05.cpp
@@ -5,3 +5,25 @@
 */
 // 참고로, 섭씨 온도를 화씨 온도로 변환하는 공식은 다음과 같다.
 // 화씨 온도 = 1.8 X 섭씨 온도 + 32.0
+
+#include <iostream>
+
+float Fahrenheit(float);    // 함수 원형 : 화씨 온도를 소수점까지 나타내기 위해서 float형으로 정해줌
+
+using namespace std;
+
+int main()
+{
+    float Celsius;  // 섭씨 온도를 소수점으로도 받을 수 있도록 float형으로 정해줌
+    cout << "섭씨 온도를 입력하시오 : ";
+    cin >> Celsius;
+    float Tmp = Fahrenheit(Celsius);
+    cout << "섭씨" << Celsius << "℃는 화씨로" << Tmp << "℃입니다." << endl;
+
+    return 0;
+}
+
+float Fahrenheit(float Cel)
+{
+    return 1.8 * Cel + 32.0;
+}
