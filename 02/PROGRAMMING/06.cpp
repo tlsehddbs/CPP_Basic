7d6940fa23900a3a2ef1aa49762372ab211bc77d
tlsehddbs <sdy4277@naver.com>
2021-01-04 17:17:34 +0900

    Solved

--- 02/PROGRAMMING/06.cpp
+++ 02/PROGRAMMING/06.cpp
@@ -4,3 +4,23 @@
 4.2 광년은 265608 천문 단위입니다.
 */
 // 천문 단위는 지구에서 태양까지의 평균 거리이다. (약 150,000,000km 또는 93,000,000miles) 광년은 빛이 1년 동안에 지나는 거리이다. (약 10조km 또는 6조miles) 1광년 = 63240 천문 단위이다. 태양을 지나 가장 가까운 별이 4.2광년 떨어져 있다. (listing 2.4와 같이) double형 변수를 사용하라.
+
+#include <iostream>
+
+using namespace std;
+
+void unit(double);
+
+int main()
+{
+    double LightYears;
+    cout << "광년 수를 입력하고 Enter 키를 누르십시오 : ";
+    cin >> LightYears;
+    unit(LightYears);
+    return 0;
+}
+
+void unit(double n)
+{
+    cout << n << "광년은 " << n * 63240 << "천문 단위입니다.";
+}
