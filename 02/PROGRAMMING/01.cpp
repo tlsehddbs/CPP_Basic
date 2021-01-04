a433e5f6cf362bc28e7427af366406aafbb82f0d
tlsehddbs <sdy4277@naver.com>
2021-01-04 16:11:01 +0900

    Solve problem

--- 02/PROGRAMMING/01.cpp
+++ 02/PROGRAMMING/01.cpp
@@ -1,1 +1,10 @@
 // 이름과 주소를 출력하는 c++ 프로그램을 작성하시오. (개인 정보를 보호받고 싶다면 가짜 이름과 주소를 작성하시오.)
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "이름 : 신ㄷㅇ\n주소 : 강원도 춘천시 ~~";
+    return 0;
+}
