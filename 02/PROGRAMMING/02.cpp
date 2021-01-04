f8dd1f1fc5ad768100beb4cdb8621cff12651f3a
tlsehddbs <sdy4277@naver.com>
2021-01-04 16:26:40 +0900

    Solved

--- 02/PROGRAMMING/02.cpp
+++ 02/PROGRAMMING/02.cpp
@@ -1,1 +1,15 @@
-// 거리에 대해 마일 단위로 입력을 요구하고, 그것을 킬로미터 만위로 환산하는 프로그램을 작성하시오. (1마일은 1.60934킬로미터이다.)
+// 거리에 대해 마일 단위로 입력을 요구하고, 그것을 킬로미터 단위로 환산하는 프로그램을 작성하시오. (1마일은 1.60934킬로미터이다.)
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int miles;
+    cin >> miles;
+    double km;
+    km = miles * 1.60934;
+    cout << miles << "마일은 " << km << "킬로미터 입니다." << endl;
+
+    return 0;
+}
