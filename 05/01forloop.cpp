2e1e0567989565190f9f465e78e1042802852294
tlsehddbs <sdy4277@naver.com>
2021-01-10 21:49:18 +0900

    Listing 5

---
+++ 05/01forloop.cpp
@@ -0,0 +1,12 @@
+// forloop.cpp -- for 루프
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int i;      // 카운터를 선언한다
+    // 초기화; 조건 검사; 갱신
+    for (i = 0; i < 5; i++)
+        cout << "C++가 루프를 사용합니다.\n";
+    cout << "루프를 언제 끝내야 하는지 C++는 알고 있습ㄴ티다.\n";
+    return 0;
+}
