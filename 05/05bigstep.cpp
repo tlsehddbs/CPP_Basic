51a1334379f80f8cc6e476f9ba4d0c286edab8c0
tlsehddbs <sdy4277@naver.com>
2021-01-10 22:19:26 +0900

    Listing 5

---
+++ 05/05bigstep.cpp
@@ -0,0 +1,15 @@
+// bigstep.cpp -- count as directed
+#include <iostream>
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    cout << "정수를 하나 입력하십시오 : ";
+    int by;
+    cin >> by;
+    cout << "갱신 크기 " << by << "s:\n";
+    for(int i = 0; i , 100; i = i + by)
+        cout << i << endl;
+    return 0;
+}
