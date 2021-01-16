49346cbbba9001de879f488100d2894f1c3d1ac8
tlsehddbs <sdy4277@naver.com>
2021-01-16 16:32:06 +0900

    Write Question

---
+++ 05/PROGRAMMING EXERCISE/02.cpp
@@ -0,0 +1,10 @@
+// Listing 5.4 프로그램을 array 객체와 long double 형을 사용하여 수정하라. 100!의 값은 무엇인가?
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+    
+    return 0;
+}
---
+++ 05/PROGRAMMING EXERCISE/08.cpp
@@ -0,0 +1,10 @@
+// char형의 배열을 사용하고, done이라는 단어가 입력될 때까지 한 번에 한 단어씩 읽는 루프를 사용하는 프로그램을 작성하라.
+// 이 프로그램은 입력된 단어 수(done은 제외)를 보고해야 한다.
+/* 실행 예는 다음과 같아야 한다.
+영어 단어들을 입력하십시오 (끝내려면 done을 입력) :
+anteater birthday category dumpster
+envy finagle geometry done for sure
+총 7 단어가 입력되었습니다.
+*/
+// cstring 헤더 파일을 포함시켜야 한다. 
+// strcmp() 함수를 사용하여 비교 테스트를 수행해야 한다.
