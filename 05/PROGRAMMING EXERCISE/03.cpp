b29806b8c285038f14781e3c7316e0a80a1e76f7
tlsehddbs <sdy4277@naver.com>
2021-01-16 16:52:01 +0900

    Solved

--- 05/PROGRAMMING EXERCISE/03.cpp
+++ 05/PROGRAMMING EXERCISE/03.cpp
@@ -1,3 +1,23 @@
 // 사용자에게 수를 입력하라고 요구하는 프로그램을 작성하라. 
 // 수를 입력할 때마다 프로그램은 그때까지 입력된 수들의 누계를 출력해야 한다. 
 // 사용자가 0을 입력하면 프로그램은 종료된다.
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int sum = 0;
+    int input;
+
+    cout << "수를 입력하십시오(끝내려면 0을 입력하십시오) : ";
+    cin >> input;
+
+    while (input != 0)
+    {
+        sum += input;
+        cout << "현재까지 입력된 수의 합 = " << sum << endl;
+        cout << "더할 값 입력(끝내려면 0) : ";
+        cin >> input;
+    }
+    return 0;
+}
--- 05/PROGRAMMING EXERCISE/10.cpp
+++ 05/PROGRAMMING EXERCISE/10.cpp
@@ -2,10 +2,27 @@
 // 첫 번째 행에 별표1개, 두 전째 행에 별표 2개, 이런 식으로 지정한 행 수만큼 출력한다.
 // 각 행에 출력되는 별표 앞에는 모든 행이 같은 수의 문자를 출력하고록 마침표로 채운다. 
 /* 프로그램 실행 예는 다음과 같아야 한다.
-출력한 행 수를 입력하십시오 : 5
+출력할 행 수를 입력하십시오 : 5
 ....*
 ...**
 ..***
 .****
 *****
 */
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int lines, i, j;
+    cout << "출력할 행 수를 입력하시오 : ";
+    cin >> lines;
+    for (int i = 0; i <= lines; i++)
+        for (int j = 1; j < lines-1; j++)
+            cout << ".";
+        for (int j = 0; j <= i; j++)
+            cout << "*";
+        cout << endl;
+
+    return 0;
+}
